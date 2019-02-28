dedb78839c405ceb4218822850a04bcac79ff5ec
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-02-27 20:35:45 -0800

    Implement the delete functionality on our todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,24 @@
 //Check Off specific todos by clicking 
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("clickList");
 })
 
 //Click on X to delete the todo
-$("span").on("click", function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
+})
+
+//
+$("input[type = 'text']").keypress(function (event) {
+    if (event.which === 13) {
+        //grabbing new Todo from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to todo
+        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+
+    }
 })
